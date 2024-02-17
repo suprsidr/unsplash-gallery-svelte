281078a3f448a895c5e0766584378d6be3c7c629
suprsidr <wayne.patterson@ninjatrader.com>
2024-02-17 12:56:43 -0500

    Masonry component

--- src/lib/components/Masonry.svelte
+++ src/lib/components/Masonry.svelte
@@ -1,142 +1,43 @@
 <script>
 	// @ts-nocheck
-	import { onDestroy, onMount, tick } from 'svelte';
-	export let stretchFirst = false,
-		gridGap = '0.5em',
-		colWidth = 'minmax(Min(20em, 100%), 1fr)',
-		items = [], // pass in data if it's dynamically updated
-		reset = '';
-
-	let grids = [],
-		masonryElement;
-
-	$: if (reset) {
-		masonryElement = masonryElement;
-	}
-
-	export const refreshLayout = async () => {
-		// console.log('REFRESHING LAYOUT');
-		grids.forEach(async (grid) => {
-			/* get the post relayout number of columns */
-			let ncol = getComputedStyle(grid._el).gridTemplateColumns.split(' ').length;
-
-			grid.items.forEach((c) => {
-				let new_h = c.getBoundingClientRect().height;
-
-				if (new_h !== +c.dataset.h) {
-					c.dataset.h = new_h;
-					grid.mod++;
-				}
-			});
-
-			/* if the number of columns has changed */
-			if (grid.ncol !== ncol || grid.mod) {
-				/* update number of columns */
-				grid.ncol = ncol;
-				/* revert to initial positioning, no margin */
-				grid.items.forEach((c) => c.style.removeProperty('margin-top'));
-				/* if we have more than one column */
-				if (grid.ncol > 1) {
-					grid.items.slice(ncol).forEach((c, i) => {
-						let prev_fin =
-								grid.items[i].getBoundingClientRect().bottom /* bottom edge of item above */,
-							curr_ini = c.getBoundingClientRect().top; /* top edge of current item */
-
-						c.style.marginTop = `${prev_fin + grid.gap - curr_ini}px`;
-					});
-				}
-
-				grid.mod = 0;
-			}
-		});
-	};
-
-	const calcGrid = async (_masonryArr) => {
-		await tick();
-		if (_masonryArr.length && getComputedStyle(_masonryArr[0]).gridTemplateRows !== 'masonry') {
-			grids = _masonryArr.map((grid) => {
-				return {
-					_el: grid,
-					gap: parseFloat(getComputedStyle(grid).gridRowGap),
-					items: [...grid.childNodes].filter(
-						(c) => c.nodeType === 1 && +getComputedStyle(c).gridColumnEnd !== -1
-					),
-					ncol: 0,
-					mod: 0
-				};
-			});
-			refreshLayout(); /* initial load */
-		}
-	};
-
-	let _window;
-	onMount(() => {
-		_window = window;
-		_window.addEventListener('resize', refreshLayout, false); /* on resize */
-	});
-	onDestroy(() => {
-		if (_window) {
-			_window.removeEventListener('resize', refreshLayout, false); /* on resize */
-		}
-	});
-
-	$: if (masonryElement) {
-		calcGrid([masonryElement]);
-	}
-
-	$: if (items) {
-		// update if items are changed
-		masonryElement = masonryElement; // refresh masonryElement
-	}
+	export let photos, searchTerm;
+	import { chunkArray } from '$lib/utils';
+	$: items = chunkArray(photos);
 </script>
 
-<!--
-  An almost direct copy and paste of: https://css-tricks.com/a-lightweight-masonry-solution
-
-  Usage:
-    - stretchFirst stretches the first item across the top
-
-  <Masonry stretchFirst={true} >
-    {#each data as o}
-      <div class="_card _padding">
-        Here's some stuff {o.name}
-        <header>
-          <h3>{o.name}</h3>
-        </header>
-        <section>
-          <p>{o.text}</p>
-        </section>
-      </div>
-    {/each}
-  </Masonry>
- -->
-
-<div
-	bind:this={masonryElement}
-	class={`__grid--masonry ${stretchFirst ? '__stretch-first' : ''}`}
-	style={`--grid-gap: ${gridGap}; --col-width: ${colWidth};`}
->
-	<slot />
+<h1>{searchTerm.toUpperCase()}</h1>
+<div class="wrapper">
+	{#each [0, 1, 2] as idx}
+		<div>
+			{#each items[idx] as photo}
+				<div class="container">
+					<a href={`/photos/${searchTerm}/${photo.slug}`}
+						><img
+							src={photo.urls.small}
+							alt={photo.description || photo.alt_description || 'No description'}
+						/></a
+					>
+				</div>
+			{/each}
+		</div>
+	{/each}
 </div>
 
-<!--
-  $w: var(--col-width); // minmax(Min(20em, 100%), 1fr);
-  $s: var(--grid-gap); // .5em;
- -->
-
 <style>
-	:global(.__grid--masonry) {
+	.container {
+		margin-bottom: 8px;
+	}
+	.container img {
+		max-width: 100%;
+		border-radius: 5px;
+		object-fit: contain;
+		box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+	}
+	.wrapper {
+		max-width: 100%;
+		margin: 0 auto;
 		display: grid;
-		grid-template-columns: repeat(auto-fit, var(--col-width));
-		grid-template-rows: masonry;
-		justify-content: center;
-		grid-gap: var(--grid-gap);
-		padding: var(--grid-gap);
-	}
-	:global(.__grid--masonry > *) {
-		align-self: start;
-	}
-	:global(.__grid--masonry.__stretch-first > *:first-child) {
-		grid-column: 1/ -1;
+		grid-template-columns: 1fr 1fr 1fr;
+		gap: 10px;
 	}
 </style>
