e6ac8b78a63ab34a3502d960621740dcb5e73c93
suprsidr <wayne.patterson@ninjatrader.com>
2024-02-26 10:01:28 -0500

    new content

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,24 +3,74 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="text-column">
+<div class="text-pumn">
 	<h1>About this app</h1>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
+	<div>
+		<h3>Hey, I'm Wayne</h3>
+		<p>Software Engineer located in the Indianapolis Area.</p>
+	</div>
+	<div>
+		<h4>This app is the product of a code challenge for a job that I was interested in.</h4>
+		<p>Requirements:</p>
+		<ul>
+			<li>Use your favourite framework: React, Angular, Vue, Express, Rails, etc</li>
+			<li>Unsplash API.</li>
+			<li>
+				Grid of photos should preserve the aspect ratio of the photos it's displaying, meaning you
+				shouldn't crop the image in any way.
+			</li>
+			<li>Design should be responsive to work in desktop and mobile</li>
+			<li>Grid should work in both portrait and landscape orientations of the device.</li>
+			<li>Grid should support pagination, i.e. you can scroll on grid of photos infinitely.</li>
+			<li>
+				When user taps on a photo on the grid it should show the photo in full screen with more
+				information about the photo.
+			</li>
+			<li>
+				Full screen mode should show ardivs on the left and right edges of the photo when user moves
+				the mouse or touches the screen.
+			</li>
+			<li>
+				When user clicks on the ardiv on a photo in full screen, it should show the the
+				next/previous photo and preserve current photos location on the grid, so when she dismisses
+				the full screen, grid of photos should contain the last photo she saw.
+			</li>
+		</ul>
+	</div>
+	<div>
+		<h4>I overshot the goal a bit.</h4>
+		<p>I tend to have fun building apps like this and get carried away.</p>
+		<p>Extras:</p>
+		<ul>
+			<li>Ability to search Unsplash plections and/or a raw image search.</li>
+			<li>User plections.</li>
+			<li>Related plections.</li>
+			<li>
+				Use AWS{' '}
+				<a href="https://github.com/suprsidr/unsplash-lambda" title="unsplash-lambda">
+					Lambda
+				</a>{' '}
+				for my API
+			</li>
+			<li>
+				Gave{' '}
+				<a href="https://recoiljs.org/" title="Recoil.js"> Recoil.js </a>{' '}
+				a try - a new way of managing state in React apps
+			</li>
+			<li>Theme switcher.</li>
+			<li>Bought cats-or-dogs.app domain :)</li>
+			<li>
+				Hosted on{' '}
+				<a href="https://www.netlify.com/" title="Netlify"> Netlify </a>
+			</li>
+		</ul>
+		<p>
+			Find the{' '}
+			<a href="https://github.com/suprsidr/unsplash-gallery-svelte" title="unsplash-gallery-svelte">
+				repo
+			</a>{' '}
+			on Github.
+		</p>
+	</div>
 </div>
